37eaa7760f9aad481f3d47292531f315df53b383
Ali Utku Pekel <utkupekel07@gmail.com>
2026-04-18 18:31:35 +0300

    Setup initial project skeleton and requirements
    Created basic folder structure (src, data) and added tech stack requirements. Fixes #1

---
+++ main.py
@@ -0,0 +1,3 @@
+print("VeriCite Framework initialization started...")
+print("Loading configurations and setting up agents...")
+# TODO: Initialize Retriever, Synthesiser, NLI Verifier, and Formal Arbiter
